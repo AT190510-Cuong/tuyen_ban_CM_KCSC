cbea5231049e185cbe55606c6ac1be3ad9590078
AT190510-Cuong <AT190510@actvn.edu.vn>
2024-12-03 16:36:33 +0700

    feat: up

--- Task_4/New folder/web/app/config.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-
-const Config = {
-    JWT_SECRET: process.env.JWT_SECRET || "cryptocatisthebest!",
-    JWT_ALGORITHM: "RS256",
-    DATABASE_URL: `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}/${process.env.POSTGRES_DB}`,
-};
-
-module.exports = Config;
